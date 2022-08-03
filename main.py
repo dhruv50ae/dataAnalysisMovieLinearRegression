2e75c4361d6f5d119e6490296b9bfe8b8c2eddf5
dhruv50ae <linkdp2012@gmail.com>
2022-08-03 12:25:59 +0100

    Basic graphs

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 from pandas import *
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 
-data = read_csv("costRevenue.csv.csv")
+data = read_csv("costRevenue.csv")
 
-x = DataFrame(data, columns=["productionBudgetUSD"])
+print(data.describe())
+
+X = DataFrame(data, columns=["productionBudgetUSD"])
 y = DataFrame(data, columns=["worldwideGrossUSD"])
 
-# plt.figure(figsize=(10, 6))
-# plt.scatter(x, y, alpha=0.3)
-# plt.title("Film Cost vs Global Revenue")
-# plt.xlabel("Production Budget ($)")
-# plt.ylabel("Worldwide Gross ($)")
-# plt.ylim(0, 3000000000)
-# plt.xlim(0, 450000000)
-# plt.show()
-
-regression = LinearRegression()
-regression.fit(x, y)
-gradient = regression.coef_
-print(gradient)
-intercept = regression.intercept_
-print(intercept)
-
 plt.figure(figsize=(10, 6))
-plt.scatter(x, y, alpha=0.3)
-plt.plot(x, regression.predict(x), color="red", linewidth=4)
-plt.title("Film Cost vs Global Revenue")
-plt.xlabel("Production Budget ($)")
-plt.ylabel("Worldwide Gross ($)")
+plt.scatter(X, y, alpha=0.5)
+plt.title("Film Budget vs Global Revenue")
+plt.xlabel("Worldwide Gross $")
+plt.ylabel("Production Budget $")
 plt.ylim(0, 3000000000)
 plt.xlim(0, 450000000)
 plt.show()
-
-print(regression.score(x, y))
