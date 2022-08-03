e14d91f8643dafa827141d007ea0d39d04fc3617
dhruv50ae <linkdp2012@gmail.com>
2022-08-03 14:23:20 +0100

    Goodness of fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pandas import *
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 data = read_csv("costRevenue.csv")
 
@@ -8,11 +9,26 @@ print(data.describe())
 X = DataFrame(data, columns=["productionBudgetUSD"])
 y = DataFrame(data, columns=["worldwideGrossUSD"])
 
+regression = LinearRegression()
+regres = regression.fit(X, y)
+print(regres)
+# Slope Coefficient
+coefficient = regression.coef_
+print(coefficient)
+# Line intercept
+intercept = regression.intercept_
+print(intercept)
+
 plt.figure(figsize=(10, 6))
 plt.scatter(X, y, alpha=0.5)
+plt.plot(X, regression.predict(X), color="red")
 plt.title("Film Budget vs Global Revenue")
 plt.xlabel("Worldwide Gross $")
 plt.ylabel("Production Budget $")
 plt.ylim(0, 3000000000)
 plt.xlim(0, 450000000)
 plt.show()
+
+goodnessOfFit = regression.score(X, y)
+
+print(goodnessOfFit)
